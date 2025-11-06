d9b3198f8ac1205f840ea465ff43e4de5c2da8a6
Lunaria <78156228+Wakaru860@users.noreply.github.com>
2025-11-06 05:26:15 +0000

    Revised the  domain list

--- LunariasAdblocker/background.js
+++ LunariasAdblocker/background.js
@@ -1,42 +1,53 @@
 const blockedDomains = [
-*://*doubleclick.net/*
-*://*googleadservices.com/*
-*://*googlesyndication.com/*
-*://*adnxs.com/*
-*://*criteo.com/*
-*://*.amazon-adsystem.com/*
-*://*.marketgid.com/*
-*://*.mathtag.com/*
-*://*.bidr.io/*
-*://*.krxd.net/*
-*://*.bluekai.com/*
-*://*.adsrvr.org/*
-*://*.taboola.com/*
-*://*.outbrain.com/*
-*://*.pubmatic.com/*
-*://*.openx.net/*
-*://*.rubiconproject.com/*
-*://*.indexexchange.com/*
-*://*.teads.tv/*
-*://*.spotxchange.com/*
-*://*.epom.com/*
-*://*.sovrn.com/*
-*://*.adform.net/*
-*://*.yieldmo.com/*
-*://*.gumgum.com/*
-*://*.triplelift.com/*
-*://*.revcontent.com/*
-*://*.appnexus.com/*
-*://*.connectad.io/*
-*://*.zedo.com/*
-*://*.adroll.com/*
-*://*.tiktok.com/*   # TikTok ads/tracking
-*://*.facebook.com/*  # Meta/Facebook Pixel & ads
-*://*.instagram.com/* # Meta ads
-*://*.linkedin.com/*  # LinkedIn Insight Tag
-*://*.twitter.com/*   # Twitter ads
+  // Google / DoubleClick
+  "*://ad.doubleclick.net/*",
+  "*://pagead2.googlesyndication.com/*",
+  "*://tpc.googlesyndication.com/*",
+  "*://*.googleadservices.com/pagead/*",
 
-  
+  // Other ad networks
+  "*://*.adnxs.com/*",
+  "*://*.criteo.com/*",
+  "*://*.mathtag.com/*",
+  "*://*.bidr.io/*",
+  "*://*.krxd.net/*",
+  "*://*.bluekai.com/*",
+  "*://*.adsrvr.org/*",
+  "*://*.pubmatic.com/*",
+  "*://*.openx.net/*",
+  "*://*.rubiconproject.com/*",
+  "*://*.indexexchange.com/*",
+  "*://*.teads.tv/*",
+  "*://*.spotxchange.com/*",
+  "*://*.epom.com/*",
+  "*://*.sovrn.com/*",
+  "*://*.adform.net/*",
+  "*://*.yieldmo.com/*",
+  "*://*.gumgum.com/*",
+  "*://*.triplelift.com/*",
+  "*://*.revcontent.com/*",
+  "*://*.appnexus.com/*",
+  "*://*.connectad.io/*",
+  "*://*.zedo.com/*",
+  "*://*.adroll.com/*",
+
+  // Social platform ads (paths only, as to not break sites)
+  "*://*.tiktok.com/*/ads/*",
+  "*://*.facebook.com/*/ads/*",
+  "*://*.instagram.com/*/ads/*",
+  "*://*.linkedin.com/*/ads/*",
+  "*://*.twitter.com/*/ads/*"
+];
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    console.log("Blocked:", details.url);
+    return { cancel: true };
+  },
+  { urls: blockedDomains },
+  ["blocking"]
+);
+
 ];
 
 chrome.webRequest.onBeforeRequest.addListener(
