b26f9b9b8a095b039957792e6da8ef49580359e4
Lunaria <78156228+Wakaru860@users.noreply.github.com>
2025-11-06 05:21:51 +0000

    Added ad domains to block and fixed issues regarding site features breaking

--- LunariasAdblocker/background.js
+++ LunariasAdblocker/background.js
@@ -1,8 +1,42 @@
 const blockedDomains = [
-  "*://*.doubleclick.net/*",
-  "*://*.googlesyndication.com/*",
-  "*://*.adservice.google.com/*",
-  "*://*.ads.yahoo.com/*"
+*://*doubleclick.net/*
+*://*googleadservices.com/*
+*://*googlesyndication.com/*
+*://*adnxs.com/*
+*://*criteo.com/*
+*://*.amazon-adsystem.com/*
+*://*.marketgid.com/*
+*://*.mathtag.com/*
+*://*.bidr.io/*
+*://*.krxd.net/*
+*://*.bluekai.com/*
+*://*.adsrvr.org/*
+*://*.taboola.com/*
+*://*.outbrain.com/*
+*://*.pubmatic.com/*
+*://*.openx.net/*
+*://*.rubiconproject.com/*
+*://*.indexexchange.com/*
+*://*.teads.tv/*
+*://*.spotxchange.com/*
+*://*.epom.com/*
+*://*.sovrn.com/*
+*://*.adform.net/*
+*://*.yieldmo.com/*
+*://*.gumgum.com/*
+*://*.triplelift.com/*
+*://*.revcontent.com/*
+*://*.appnexus.com/*
+*://*.connectad.io/*
+*://*.zedo.com/*
+*://*.adroll.com/*
+*://*.tiktok.com/*   # TikTok ads/tracking
+*://*.facebook.com/*  # Meta/Facebook Pixel & ads
+*://*.instagram.com/* # Meta ads
+*://*.linkedin.com/*  # LinkedIn Insight Tag
+*://*.twitter.com/*   # Twitter ads
+
+  
 ];
 
 chrome.webRequest.onBeforeRequest.addListener(
